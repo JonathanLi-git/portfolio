66cfc4fc5aa8532699308809498551041297ff21
JonathanLi-git <li.brothers.j@gmail.com>
2021-07-10 17:20:58 -0400

    heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'g-=7!xh#fcpc*p$e9ylo36ds@*=3=916ck%io7l(#&+#3s6y9w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
